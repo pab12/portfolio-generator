66125d6f40cefe8b98657b027657e787d95f0b5c
Paul Barcenas <paulbarcenas@yahoo.com>
2021-09-29 18:35:05 -0500

    prompt the user for more input

--- app.js
+++ app.js
@@ -11,11 +11,81 @@ const inquirer = require('inquirer');
 // });
 
 inquirer
-.prompt([
+const promptUser = () => {
+  return inquirer.prompt([
   {
     type: 'input',
     name: 'name',
     message: 'what is your name?'
+  },
+  {type: 'input',
+    name: 'github',
+    message: 'Enter your Github Username'
+  },
+  {
+    type: 'input',
+    name: 'about',
+    message: 'PRovide some information about yourself:'
   }
-])
-.then(answers => console.log(answers));
+]);
+};
+
+
+const promptProject = portfolioData => {
+  console.log(`
+  =================
+  Add a New Project
+  =================
+  `);
+   // If there's no 'projects' array property, create one
+if (!portfolioData.projects) {
+  portfolioData.projects = [];
+}
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name:'name',
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['javaScript', 'HTML', 'CSS', 'ES6', 'jquery', 'Bootstrap','Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)'
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
+    }
+    
+  ]).then(projectData => {
+    portfolioData.projects.push(projectData);
+    if (projectData.confirmAddProject){
+      return promptProject(portfolioData);
+    } else {
+      return portfolioData;
+    }
+  });
+ 
+}
+
+
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    console.log(portfolioData);
+  });
+
+
